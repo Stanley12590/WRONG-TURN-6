6f5693e65f51266daa6d6882a81f7565a5cd44eb
Stanley12590 <masanyiwastanley378@gmail.com>
2026-01-16 01:40:56 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    botName: "WRONG TURN 6",
+    ownerName: "STANYTZ",
+    ownerNumber: "255618558502", 
+    prefix: ".",
+    mongoUri: "mongodb+srv://STANY:STANY@cluster0.ennpt6t.mongodb.net/?appName=Cluster0",
+    channelLink: "https://whatsapp.com/channel/0029Vb7fzu4EwEjmsD4Tzs1p",
+    groupLink: "https://chat.whatsapp.com/J19JASXoaK0GVSoRvShr4Y",
+    menuImage: "https://i.ibb.co/vz6mD7y/wrongturn.jpg"
+};
