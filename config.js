d2690719d9cb8922e7907240b14595d681140b49
Stanley12590 <masanyiwastanley378@gmail.com>
2026-01-16 05:08:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ module.exports = {
     mongoUri: "mongodb+srv://stanytz076:stanytz076@cluster0.ennpt6t.mongodb.net/WrongTurn6?retryWrites=true&w=majority",
     channelLink: "https://whatsapp.com/channel/0029Vb7fzu4EwEjmsD4Tzs1p",
     groupLink: "https://chat.whatsapp.com/J19JASXoaK0GVSoRvShr4Y",
-    groupId: "J19JASXoaK0GVSoRvShr4Y@g.us", // Your Group JID
-    menuImage: "https://files.catbox.moe/jdtfqg.jpeg"
+    groupId: "J19JASXoaK0GVSoRvShr4Y@g.us", // Use .jid command later to get the exact ID
+    menuImage: "https://i.ibb.co/vz6mD7y/wrongturn.jpg"
 };
