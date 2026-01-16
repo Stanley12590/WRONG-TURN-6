228bea6a3c578f7f33f011ea24b1b94e70fc018e
Stanley12590 <masanyiwastanley378@gmail.com>
2026-01-16 04:21:27 +0300

    Update database.js

--- database.js
+++ database.js
@@ -3,8 +3,12 @@ const UserSchema = new mongoose.Schema({
     id: { type: String, unique: true },
     name: String,
     verified: { type: Boolean, default: false },
+    antiLink: { type: Boolean, default: true },
     antiDelete: { type: Boolean, default: true },
-    autoRead: { type: Boolean, default: false }
+    autoStatusView: { type: Boolean, default: true },
+    autoStatusLike: { type: Boolean, default: true },
+    autoStatusReply: { type: Boolean, default: false },
+    statusReplyMsg: { type: String, default: "Nice status! Viewed by WRONG TURN 6 ✔️" }
 });
 const User = mongoose.model('WT6_User', UserSchema);
 module.exports = { User };
