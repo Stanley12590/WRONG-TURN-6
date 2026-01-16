09f38e3afaadb01683a7d56d8ecd2b543fe334fa
Stanley12590 <masanyiwastanley378@gmail.com>
2026-01-16 11:18:41 +0300

    Update database.js

--- database.js
+++ database.js
@@ -9,12 +9,12 @@ const UserSchema = new mongoose.Schema({
     id: { type: String, unique: true },
     name: String,
     antiDelete: { type: Boolean, default: true },
-    antiViewOnce: { type: Boolean, default: true },
+    antiLink: { type: Boolean, default: true },
     autoStatus: { type: Boolean, default: true },
-    antiLink: { type: Boolean, default: true }
+    verified: { type: Boolean, default: true }
 });
 
-const Session = mongoose.model('WT6_Session', SessionSchema);
-const User = mongoose.model('WT6_User', UserSchema);
+const Session = mongoose.model('Session_WT6', SessionSchema);
+const User = mongoose.model('User_WT6', UserSchema);
 
 module.exports = { Session, User };
