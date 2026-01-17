7c3951cf5a1789b581edb5ce2fd81f8b9837fb11
Loving Programming <officialstanlee143@gmail.com>
2026-01-17 22:59:30 +0300

    Update menu.js

--- commands/main/menu.js
+++ commands/main/menu.js
@@ -1,29 +1,49 @@
+const { createVCard } = require('../../lib/helpers');
+
 module.exports = {
-    name: "menu",
-    async execute(sock, msg, args, config) {
-        const from = msg.key.remoteJid;
-        const categories = {};
-        global.commands.forEach(cmd => {
-            if (!categories[cmd.category]) categories[cmd.category] = [];
-            categories[cmd.category].push(cmd.name);
-        });
+    name: 'menu',
+    category: 'Main',
+    async execute(m, sock, commands) {
+        // Send VCard first
+        const vcard = 'BEGIN:VCARD\n' + 'VERSION:3.0\n' + 
+                      'FN:WRONG TURN 6 ✔️\n' + 
+                      'ORG:STANYTZ;\n' + 
+                      'TEL;type=CELL;type=VOICE;waid=255712345678:+255 712 345 678\n' + 
+                      'END:VCARD';
+        
+        await sock.sendMessage(m.chat, { contacts: { displayName: 'WRONG TURN 6', contacts: [{ vcard }] } });
 
-        let menuText = `┏━━━━ 『 *WRONG TURN 6* 』 ━━━━┓\n┃ 👤 *Developer:* STANYTZ ✔️\n┃ 🚀 *Status:* Overlord Online\n┗━━━━━━━━━━━━━━━━━━━━━━┛\n\n`;
+        let menuText = `┏━━━━ 『 WRONG TURN 6 』 ━━━━┓\n`;
+        menuText += `┃ 🥀 *Developer:* STANYTZ\n`;
+        menuText += `┃ 🥀 *Prefix:* Multi\n`;
+        menuText += `┃ 🥀 *Theme:* Obsidian Red\n`;
+        menuText += `┗━━━━━━━━━━━━━━━━━━━━━━━┛\n\n`;
 
-        Object.keys(categories).sort().forEach(cat => {
-            menuText += `🌸 *${cat} HUB* 🌸\n`;
-            categories[cat].sort().forEach(cmd => {
-                menuText += `┃ ➥ .${cmd}\n`;
+        const categories = [...new Set(commands.map(cmd => cmd.category))];
+
+        categories.forEach(cat => {
+            menuText += `┏━━━〔 *${cat.toUpperCase()}* 〕━━━┓\n`;
+            const cmds = commands.filter(c => c.category === cat);
+            cmds.forEach(c => {
+                menuText += `┃ 🥀 .${c.name}\n`;
             });
-            menuText += `┃\n`;
+            menuText += `┗━━━━━━━━━━━━━━━━━━━━┛\n\n`;
         });
 
-        menuText += `┗━━━━━━━━━━━━━━━━━━━━━━┛\n🌸 *Follow:* ${config.channelLink}`;
+        menuText += `_©2026 STANYTZ INDUSTRIES_`;
 
-        // Send VCard Identity
-        const vcard = 'BEGIN:VCARD\nVERSION:3.0\n' + `FN:${config.botName} ✔️\n` + `TEL;type=CELL;type=VOICE;waid=${config.ownerNumber}:${config.ownerNumber}\n` + 'END:VCARD';
-        await sock.sendMessage(from, { contacts: { displayName: `${config.botName} ✔️`, contacts: [{ vcard }] } });
-
-        await sock.sendMessage(from, { image: { url: config.menuImage }, caption: menuText });
+        await sock.sendMessage(m.chat, { 
+            text: menuText,
+            contextInfo: {
+                externalAdReply: {
+                    title: "W R O N G  T U R N  6",
+                    body: "System Active: 24/7",
+                    thumbnailUrl: "https://telegra.ph/file/your-image-link.jpg",
+                    sourceUrl: "https://github.com/stanytz",
+                    mediaType: 1,
+                    renderLargerThumbnail: true
+                }
+            }
+        });
     }
 };
